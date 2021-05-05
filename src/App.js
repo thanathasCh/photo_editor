cb229ce9c4591854db1f8bf0a5197bd18ee73361
thanathas.ch <thanathasCh@gmail.com>
2021-05-05 18:03:39 +0700

    Add init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
+import Slider from "./components/Slider";
+import SidebarItem from "./components/SidebarItem";
+import EditorOptions from "./models/EditorOptions";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [options, setOptions] = useState(EditorOptions());
+    const [selectedOptionIndex, setSelectedOptionIndex] = useState(0);
+    const selectedOption = options[selectedOptionIndex];
+
+    function handleSliderChange({ target }) {
+        setOptions(prevOptions => {
+            return prevOptions.map( (option, index) => {
+                if (index !== selectedOptionIndex) {
+                    return option
+                } else {
+                    return { ...option, value: target.value }
+                }
+            })
+        })
+    }
+
+    function getImageStyle() {
+        const filters = options.map(option => {
+            return `${option.property}(${option.value}${option.unit})`;
+        })
+
+        return { filter: filters.join(' ')};
+    }
+
+    return (
+        <div className="container">
+            <div className="main-image" style={getImageStyle()}/>
+            <div className="sidebar">
+                {
+                    options.map( (option, index) => {
+                        return (
+                            <SidebarItem
+                                key={index}
+                                name={option.name}
+                                active={index === selectedOptionIndex}
+                                handleClick={ () => setSelectedOptionIndex(index)}
+                            />
+                        )
+                    })
+                }
+            </div>
+
+            <Slider
+                min={selectedOption.range.min}
+                max={selectedOption.range.max}
+                value={selectedOption.value}
+                handleChange={handleSliderChange}
+            />
+        </div>
+    )
 }
 
 export default App;
